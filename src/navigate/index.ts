99169af1d9c243dca60d459cbd05f0a462aff783
Andrii <jitelgor@gmail.com>
2025-06-15 01:55:02 +0100

    wip

--- src/navigate/index.ts
+++ src/navigate/index.ts
@@ -1,8 +1,12 @@
 import type { RouteBase } from '../createRouter';
 import { ROUTE_METHODS, ROUTE_PARAMS } from '../utils/constants';
 
-const navigate = (to: RouteBase<true>, replace?: boolean) => {
-  to[ROUTE_METHODS]._navigate(null, to[ROUTE_PARAMS], replace);
+const navigate = (
+  to: RouteBase<true>,
+  replace?: boolean,
+  ignoreBlock?: boolean
+) => {
+  to[ROUTE_METHODS]._navigate(null, to[ROUTE_PARAMS], replace, ignoreBlock);
 };
 
 export default navigate;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,9 @@ export const ROOT = Symbol();
 export const ROUTE_METHODS = Symbol();
 
 export const ROUTE_PARAMS = Symbol();
+
+export const BLOCK_ROUTER = Symbol();
+
+export const ROUTER = Symbol();
+
+export const EMPTY_OBJECT = {};
